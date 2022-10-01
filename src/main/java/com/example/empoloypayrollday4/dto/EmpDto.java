53722f430ced249c8eac9df2fea6adbd4e467680
Anicod <animeshraj735@gmail.com>
2022-10-01 09:38:49 +0530

    added: lombok processor for all anotatoion such as getter setter and contructor

--- src/main/java/com/example/empoloypayrollday4/dto/EmpDto.java
+++ src/main/java/com/example/empoloypayrollday4/dto/EmpDto.java
@@ -1,7 +1,12 @@
 package com.example.empoloypayrollday4.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Data //data anotation uses no need to getter setter, no argument constructor and argument constructor
 public class EmpDto {
     public String firstName;
     public String lastName;
--- src/main/java/com/example/empoloypayrollday4/empmodel/EmpModel.java
+++ src/main/java/com/example/empoloypayrollday4/empmodel/EmpModel.java
@@ -1,6 +1,9 @@
 package com.example.empoloypayrollday4.empmodel;
 
 import com.example.empoloypayrollday4.dto.EmpDto;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,8 @@ import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
+@Data //data anotation uses no need to getter setter, no argument constructor and argument constructor
+
 public class EmpModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,64 +26,8 @@ public class EmpModel {
     public String note;
     public Date date;
 
-    public Date getDate() {
-        return date;
-    }
+    public EmpModel(){
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public EmpModel() {
-
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Integer salary) {
-        this.salary = salary;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
     }
 
     public EmpModel(String firstName, String lastName, String address, Integer salary, String profilePic, String note, Date date){
