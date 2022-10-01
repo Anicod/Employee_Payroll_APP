786548e171271aa72c7b8ec878fd0c47e61204f0
Anicod <animeshraj735@gmail.com>
2022-10-01 11:05:59 +0530

    added: slf4j anootation and application.log file for all warning and error

--- src/main/java/com/example/empoloypayrollday4/Empoloypayrollday4Application.java
+++ src/main/java/com/example/empoloypayrollday4/Empoloypayrollday4Application.java
@@ -1,12 +1,15 @@
 package com.example.empoloypayrollday4;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Empoloypayrollday4Application {
 
     public static void main(String[] args) {
+        log.info("hello empolyee");
         SpringApplication.run(Empoloypayrollday4Application.class, args);
     }
 
